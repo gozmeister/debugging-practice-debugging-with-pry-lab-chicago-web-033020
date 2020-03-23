3133927f9be3366ad159125f655a72234161d55e
Sam Gorick <samgorick.sg@gmail.com>
2020-03-23 20:03:45 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,8 +9,8 @@ def turtles
 end
 
 def turtle_traits(turtles)
-  turtles.each do |turtle|
-    turtle[:traits].map do |trait|
+  turtles.map do |turtle|
+    turtle[:traits].each do |trait|
       trait
     end
   end
